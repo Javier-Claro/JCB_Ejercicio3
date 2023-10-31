dbd1cc1d8185327ee55756d0cb54891c847431bd
DESKTOP-D5NS5P4\manuel <manuelclarobarba@gmail.com>
2023-10-31 23:07:04 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3/Controladores/Program.cs
@@ -0,0 +1,22 @@
+﻿using Ejercicio3.Servicios;
+
+namespace Ejercicio3.Controladores
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            MetodosInterfaz mi = new MetodosImplementacion();
+            int numPedido = mi.pedirNumero();
+
+            mi.piramide(numPedido);
+
+
+        }
+
+    }
+
+}
